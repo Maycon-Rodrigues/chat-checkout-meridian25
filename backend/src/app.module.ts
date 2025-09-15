1d5033f4e0ad918789e26cb2de173afcd1bd4be6
matheusbrasilaguiar <matheusbrasilaguiar@gmail.com>
2025-09-15 18:27:27 -0300

    feat: reorganize module imports in AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { ProductModule } from './product/product.module';
+import { MerchantModule } from './merchant/merchant.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -14,9 +17,12 @@ import { AuthModule } from './auth/auth.module';
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
       autoLoadEntities: true,
-      synchronize: true,
+      synchronize: true, // NOTE: Set to false in production
     }),
-    AuthModule,
+  AuthModule,
+  UserModule,
+  MerchantModule,
+  ProductModule,
   ],
 })
 export class AppModule {}
